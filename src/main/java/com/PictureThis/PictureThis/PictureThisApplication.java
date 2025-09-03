0bccda93928deecf21f76aa3719a251f4d51d8f3
Fatema Afroza <fafroza2017@gmail.com>
2025-09-03 09:43:58 +0200

    Testar igen

---
+++ src/main/java/com/PictureThis/PictureThis/PictureThisApplication.java
@@ -0,0 +1,13 @@
+package com.PictureThis.PictureThis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureThisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictureThisApplication.class, args);
+	}
+
+}
