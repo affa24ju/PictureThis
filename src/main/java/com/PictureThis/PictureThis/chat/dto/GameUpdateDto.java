e0b42d1c59a3cb37f11315d0eb228f99477dffff
adNord <adno96@gmail.com>
2025-09-10 12:59:20 +0200

    gameupdatedto ändrat till record, skickas till /topic/game-updates

--- src/main/java/com/PictureThis/PictureThis/chat/dto/GameUpdateDto.java
+++ src/main/java/com/PictureThis/PictureThis/chat/dto/GameUpdateDto.java
@@ -2,30 +2,4 @@ package com.PictureThis.PictureThis.chat.dto;
 
 import java.util.Map;
 
-public class GameUpdateDto {
-    private String event;
-    private Map<String, Object> content;
-
-    public GameUpdateDto() {}
-
-    public GameUpdateDto(String event, Map<String, Object> content) {
-        this.event = event;
-        this.content = content;
-    }
-
-    public String getEvent() {
-        return event;
-    }
-
-    public void setEvent(String event) {
-        this.event = event;
-    }
-
-    public Map<String, Object> getContent() {
-        return content;
-    }
-
-    public void setContent(Map<String, Object> content) {
-        this.content = content;
-    }
-}
+public record GameUpdateDto(String event, Map<String, Object> content) {}
