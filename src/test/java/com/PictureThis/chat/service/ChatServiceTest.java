50688bd2f184a24d22e8eaf4b3ff2356c5a05f1f
Fatema Afroza <fafroza2017@gmail.com>
2025-09-10 22:42:18 +0200

    Lagt till en spelare annars får null point exception

--- src/test/java/com/PictureThis/chat/service/ChatServiceTest.java
+++ src/test/java/com/PictureThis/chat/service/ChatServiceTest.java
@@ -12,6 +12,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import com.PictureThis.PictureThis.chat.model.ChatMessage;
 import com.PictureThis.PictureThis.chat.service.ChatService;
+import com.PictureThis.PictureThis.user.dto.UserDto;
 
 public class ChatServiceTest {
     private SimpMessagingTemplate messagingTemplate;
@@ -25,12 +26,40 @@ public class ChatServiceTest {
         chatService = Mockito.spy(new ChatService(messagingTemplate));
     }
 
+    // Vid rätt state & rätt gissning: ändrar state till ROUND_END
+    // anropar broadcastGameState och startRound
     @Test
     void handleGuess_CorrectGuess_ShouldChangeStateAndBroadcast() {
         // Arrange
         chatService.getGameSession().setState(ChatService.SessionState.DRAWING);
         chatService.getGameSession().setCurrentWord("apple");
 
+        // Lägger till en spelare, annars får null point exception
+        chatService.getGameSession().getPlayers().add(new UserDto("1", "kalle"));
+
+        ChatMessage guess = new ChatMessage("kalle", "apple");
+
+        // Mockar bort startRound, annars sätter den tillbaka state till Drawing
+        doNothing().when(chatService).startRound();
+        // Act
+        chatService.handleGuess(guess);
+
+        // Assert
+        assert chatService.getGameSession().getState() == ChatService.SessionState.ROUND_END;
+
+        verify(chatService).broadcastGameState(contains("Correct guess by kalle!"));
+        verify(chatService).startRound();
+
+    }
+
+    // Vid rätt state & fel gissning: ska inte hända något
+    @Test
+    void handleGuess_WrongState_ShouldDoNothing() {
+        // Arrange
+        chatService.getGameSession().setState(ChatService.SessionState.DRAWING);
+        chatService.getGameSession().setCurrentWord("banana");
+
+        // Passerar fel ord
         ChatMessage guess = new ChatMessage("kalle", "apple");
 
         // Act
@@ -39,14 +68,8 @@ public class ChatServiceTest {
         // Assert
         assert chatService.getGameSession().getState() == ChatService.SessionState.DRAWING;
 
-        verify(chatService).broadcastGameState(contains("Correct guess by kalle!"));
-        verify(chatService).startRound();
-
-    }
-
-    @Test
-    void handleGuess_WrongState_DoesNothing() {
-        // Arrange
+        verify(chatService, never()).broadcastGameState(anyString());
+        verify(chatService, never()).startRound();
 
     }
 }
