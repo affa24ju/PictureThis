e607951b42a654c21f881fc704ccf3e686f4d31e
adNord <adno96@gmail.com>
2025-09-15 10:06:08 +0200

    cors och application properties fix, kanske?

--- src/main/java/com/PictureThis/PictureThis/config/SecurityConfig.java
+++ src/main/java/com/PictureThis/PictureThis/config/SecurityConfig.java
@@ -35,10 +35,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of(
-                "http://localhost:5173",
-                "https://picturethisfrontend-production.up.railway.app"
-        ));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
--- src/main/java/com/PictureThis/PictureThis/user/controllers/UserController.java
+++ src/main/java/com/PictureThis/PictureThis/user/controllers/UserController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -22,7 +21,6 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/api/users")
-@CrossOrigin(origins = "*")
 public class UserController {
 
     @Autowired
