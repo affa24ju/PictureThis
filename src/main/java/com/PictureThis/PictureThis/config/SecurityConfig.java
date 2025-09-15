c57bba087f5a1f54bf6614374e8f75fe443622bf
adNord <adno96@gmail.com>
2025-09-15 10:17:35 +0200

    security config credentials to false

--- src/main/java/com/PictureThis/PictureThis/config/SecurityConfig.java
+++ src/main/java/com/PictureThis/PictureThis/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig {
         configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
-        configuration.setAllowCredentials(true);
+        configuration.setAllowCredentials(false);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
